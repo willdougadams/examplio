256878ff20842f456df82d5e1e41a3165aaeb799
Will Adams <will@urgentiq.com>
2024-10-19 13:08:17 -0700

    change inmport

--- api/src/functions/graphql.ts
+++ api/src/functions/graphql.ts
@@ -7,7 +7,7 @@ import services from 'src/services/**/*.{js,ts}'
 
 import { getCurrentUser } from 'src/lib/auth'
 import { db } from 'src/lib/db'
-import generateGraphiQLHeader from 'src/lib/generateGraphiQLHeader'
+import { generateGraphiQLHeader } from 'src/lib/generateGraphiQLHeader'
 import { logger } from 'src/lib/logger'
 
 export const handler = createGraphQLHandler({
